6106a7577000c921676bee1a64fcbe59f8c6c0d0
Luca Barrella <lucabarrella@icloud.com>
2024-01-19 18:42:52 +0100

    nothing

--- src/main/java/com/example/uninasocialgroup/HelloApplication.java
+++ src/main/java/com/example/uninasocialgroup/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Untitled.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());//, 320, 240);
         stage.setTitle("Unina Social Group");
         stage.resizableProperty().setValue(false);

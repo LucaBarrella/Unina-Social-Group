3d037a956d08d9e476ae3fb56b36a7aa769c5a81
Luca Barrella <lucabarrella@icloud.com>
2024-01-26 18:23:21 +0100

    Modifiche effettuate: - Implementato il gestore di connessione al database nel file DatabaseConnectionManager. Questo include la creazione, la chiusura e la stampa dello stato della connessione. Le credenziali del database sono state definite come variabili statiche finali. La connessione viene chiusa nel blocco finally per garantire che venga sempre chiusa, indipendentemente dal successo o meno della connessione.

---
+++ src/main/java/BackEnd/Class/DatabaseConnectionManager.java
@@ -0,0 +1,42 @@
+package BackEnd.Class;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectionManager {
+
+    private static final String DatabaseURL = "jdbc:postgresql://manny.db.elephantsql.com:5432/";
+    private static final String DatabaseUser = "rhwjlmoo";
+    private static final String DatabasePassword = "qRli1Cj50UXsPpSaCn4cG1u5cTgoqjZF";
+
+    public static Connection createDatabaseConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("manny.db.elephantsql.com");
+            connection = DriverManager.getConnection(DatabaseURL, DatabaseUser, DatabasePassword);
+            printConnectionStatus(connection);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeDatabaseConnection(connection);
+        }
+        return connection;
+    }
+
+    private static void printConnectionStatus(Connection connection) {
+        String connectionStatus = (connection != null) ? "Connessione al database riuscita" : "Impossibile connettersi al database";
+        System.out.println(connectionStatus);
+    }
+
+    private static void closeDatabaseConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Connessione chiusa!");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
